560522ddb27267be29a94c031520174f140b8163
topo-delft <topo.delft@outlook.com>
2018-07-08 12:33:31 +0200

    initial commit
    basic package structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='cqed',
+    version='0.0.1',
+    description='cQED experiments with qcodes in the TOPO group',
+    author='Wolfgang Pfaff',
+    author_email='wolfgangpfff@gmail.com',
+    url='https://github.com/kouwenhovenlab',
+)
