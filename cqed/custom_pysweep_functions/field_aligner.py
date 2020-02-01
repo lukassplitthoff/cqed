bca6412e9a0d0b04102ba7c55fe9f815b2479a21
lgrunhaupt <L.Grunhaupt@tudelft.nl>
2020-02-01 14:46:18 +0100

    initial commit, based on Arno's code

--- cqed/custom_pysweep_functions/field_aligner.py
+++ cqed/custom_pysweep_functions/field_aligner.py
@@ -1,144 +1,130 @@
-def setup_frq_sweep(fstart, fstop, fpts, chan='S21', bw=None, navgs=None, pwr=None):
-    """
-    Setup a VNA trace.
-    
-    assumes that a channel with name chan is already created.
-    """
-    vna = qc.Station.default.vna
-    trace = getattr(vna.channels, chan)
-    
-    fvals = np.linspace(fstart, fstop, fpts)
-    trace.start(fstart)
-    trace.stop(fstop)
-    trace.npts(fpts)
-    if navgs is not None:
-        trace.avg(navgs)
-    if bw is not None:
-        trace.bandwidth(bw)
-    if pwr is not None:
-        trace.power(pwr)
-    trace.autoscale()
-    
-    if not vna.rf_power():
-        vna.rf_on()
-    
-    return fvals
+# Automatized optimization of magnetic field alignment based on maximizing
+# the frequency of a reference resonator
+# cQED group @Kouwenhoven-lab TU Delft
 
-def take_trace(chan='S21', plot=False):
-    """
-    Get the data of a currently measured trace.
-    The trace has to be setup already.
-    
-    Returns magnitude (in dB) and phase (in rad).
-    
-    If plot is true, make a simple plot of the magnitude vs frequency.
-    """
-    vna = qc.Station.default.vna
-    trace = getattr(vna.channels, chan)
-    
-    fvals = np.linspace(trace.start(), trace.stop(), trace.npts())
-    mag, phase = trace.trace_mag_phase()
-    
-    if plot:
-        fig, ax = plt.subplots(1, 1)
-        ax.plot(fvals*1e-9, 20*np.log10(mag))
-        ax.grid(dashes=[1,1])
-        ax.set_xlabel('Frequency (GHz)')
-        ax.set_ylabel('Magnitude (dBm)')
-        
-    return mag, phase
+from cqed.custom_pysweep_functions.vna import return_vna_trace
+import numpy as np
+from time import sleep
+import matplotlib.pyplot as plt
 
-    def find_resonance(plot=False):
-    global current_objective
-    '''Crude estimate of the resonance frequency and its width. Could use a fancier fit, but would be less robust.
-    Inputs:
-    plot (boolean): plots the VNA linescan
-    '''
-    fval = setup_frq_sweep(fstart=current_objective-df, fstop=current_objective+df, fpts=501, pwr=-20, bw=1e2, navgs=1)
-    mag, phase = take_trace(plot=plot)
-    vna.rf_off()
-    f0 = fval[np.argmin(mag)] #crude resonance frequency estimate
-    return f0
 
-def find_resonance_advanced(current_objective, df, fpts=501, pwr=-20, bw=1e2, navgs=1, plot=False,chan = "S21"):
-    fstart=current_objective-df
-    fstop=current_objective+df
-    station.vna.timeout(fpts/bw*1.1)
-    fval = setup_frq_sweep(fstart, fstop, fpts, chan, bw, navgs, pwr)
-    mag, phase = take_trace(plot=plot)
-    vna.rf_off()
-    f0 = fval[np.argmin(mag)] 
-    return f0
+class FieldAligner:
 
-    def optimize_x(max_magnitude, wiggle_step, min_wiggle, observer_fn, plot=True, return_extra=True):
-    global current_objective
-    ''' Wiggle the x-axis of the magnetic field up and down until the objective is optimized to the desired accuracy.
-    Picture a Roombah, I have been told.
-    Inputs: 
-    max_magnitude (float): maximal magnetic field value for x, unit: T
-    wiggle_step (float): initial step size for changes in magnetic field, unit: T
-    min_wiggle (float): smallest field step taken after which the protocol terminates, unit: T
-    min_wiggle (float): smallest field step taken after which the protocol terminates, unit: T
-    observer_fn: a callable which gets called after each small step and measures the objective
-    '''
-    objectives_meas = []
-    locations = []
-    
-    nsteps = 1
-    direction = 1
-    
-    current_objective = observer_fn()
-    
-    objectives_meas.append(current_objective)
-    locations.append(station.mgnt.x_measured())
-    
-    while wiggle_step>min_wiggle:
-        pos = station.mgnt.x_measured()
-        if abs(pos+direction*wiggle_step) > max_magnitude:
-            raise Exception('X-axis magnitude limit reached.')
-            
-        print(current_objective*1e-9, '{0:.6f}'.format(station.mgnt.x_measured()), '->', '{0:.6f}'.format(pos+direction*wiggle_step))
-        station.mgnt.x_target(pos+direction*wiggle_step)
-        station.mgnt.ramp(mode='safe')
-        time.sleep(1.) 
-        newpos = station.mgnt.x_measured()
-        if np.abs(pos-newpos) < 0.8*wiggle_step:
-            continue
-        
-        new_objective = observer_fn()
-        objectives_meas.append(new_objective)
-        locations.append(station.mgnt.x_measured())
-        nsteps+=1
-        if new_objective < current_objective:
-            current_objective = new_objective
-            direction = -1*direction
-            if direction == 1:
-                wiggle_step=wiggle_step/2
+    def __init__(self, station=station, f0=None, search_span=25e6,
+                 frq_resolution=200e3, max_field_amp=10e-3, wiggle_step=0.4e-3,
+                 min_wiggle=0.1e-3, vna_avgs=None, vna_bw=None, vna_pwr=None):
+
+        self.d = {'STATION': station}
+        self.vna_trace = getattr(station.vna.channels, chan)
+        self.mgnt = station.mgnt
+
+        self.max_amplitude = max_field_amp
+        self.wiggle_step = wiggle_step
+        self.min_wiggle = min_wiggle
+
+        if vna_avgs is None:
+            self.vna_avgs = self.vna_trace.avg()
         else:
-            current_objective = new_objective
-        
-            
-    optimum = current_objective        
-    print('nstep', nsteps)
-    extra = {'objectives': objectives_meas, 
-            'fields': locations}
-    
-    if plot:
-        plt_xs = extra['fields']
-        plt_ys = extra['objectives']
-        plt.figure()
-        plt.plot(
-            plt_xs,
-            plt_ys,
-        )
-        plt.scatter(
-            plt_xs,
-            plt_ys,
-            c=extra['objectives']
-        )
-        plt.colorbar()
+            self.vna_avgs = vna_avgs
 
-    if return_extra:
-        return optimum, extra
-    else:
-        return optimum
+        if vna_bw is None:
+            self.vna_bw = self.vna_trace.bandwidth()
+        else:
+            self.vna_bw = vna_bw
+
+        if vna_pwr is None:
+            self.vna_pwr = self.vna_trace.power()
+        else:
+            self.vna_pwr = vna_pwr
+
+        self.current_f0 = f0
+        self.search_span = search_span
+        self.frq_resolution = frq_resolution
+
+        if f0 is None:
+            self.current_f0 = self.find_resonance()
+
+        self._target_fcn = self.find_resonance
+
+        self.objectives_meas = []
+        self.locations = []
+        self.nsteps = 1
+
+    def find_resonance(self):
+
+        self.vna_trace.start(self.current_f0 - self.search_span / 2.)
+        self.vna_trace.stop(self.current_f0 + self.search_span / 2.)
+        self.vna_trace.npts(int(self.search_span / self.frq_resolution) + 1)
+        self.vna_trace.bandwidth(self.vna_bw)
+        self.vna_trace.power(self.vna_pwr)
+        self.vna_trace.avg(self.vna_avgs)
+
+        data = return_vna_trace(self.d)
+        return data[0][np.argmin(data[1])]
+
+    def optimize_x(self):
+
+        """ Wiggle the x-axis of the magnetic field up and down until the
+        objective is optimized to the desired accuracy.
+        Picture a Roombah, I have been told.
+        Inputs:
+        max_magnitude (float): maximal magnetic field value for x, unit: T
+        wiggle_step (float): initial step for changes in magnetic field, unit: T
+        min_wiggle (float): field step taken after which
+                                the protocol terminates, unit: T
+        observer_fn: a callable which measures the objective
+        """
+
+        self.objectives_meas = []
+        self.locations = []
+
+        self.nsteps = 1
+        self.objectives_meas.append(self.current_f0)
+        self.locations.append(self.mgnt.x_measured())
+
+        direction = 1
+        wiggle_step = self.wiggle_step
+
+        while wiggle_step > self.min_wiggle:
+
+            pos = self.mgnt.x_measured()
+            if abs(pos + direction * wiggle_step) > self.max_amplitude:
+                raise Exception('X-axis magnitude limit reached.')
+
+            print('f0 = {0:.4f} GHz'.format(self.current_f0 / 1e9),
+                  '{0:.4f} T'.format(self.mgnt.x_measured()), '->',
+                  '{0:.4f} T'.format(pos + direction * wiggle_step))
+
+            self.mgnt.x_target(pos + direction * wiggle_step)
+            self.mgnt.ramp(mode='safe')
+            sleep(1.)
+            newpos = self.mgnt.x_measured()
+
+            if np.abs(pos - newpos) < 0.8 * wiggle_step:
+                continue
+
+            new_objective = self._target_fcn()
+            self.objectives_meas.append(new_objective)
+            self.locations.append(self.mgnt.x_measured())
+            self.nsteps += 1
+
+            if new_objective < self.current_f0:
+                self.current_f0 = new_objective
+                direction = -1 * direction
+
+                if direction == 1:
+                    # should we round this to 0.1mT?
+                    wiggle_step = wiggle_step / 2.
+            else:
+                self.current_f0 = new_objective
+
+        print('Optimization finished after {} steps'.format(self.nsteps),
+              '\nTarget value from {0:.4f} GHz to {0:.4f} GHz'.format(
+                  self.objectives_meas[0], self.objectives_meas[-1]),
+              '\nX-field = {0:.4f} T'.format(self.mgnt.x_measured()))
+
+    def plot_optimization(self):
+
+        plt.plot(self.locations, self.objectives_meas)
+        plt.scatter(self.locations, self.objectives_meas, c=self.objectives_meas)
+        plt.colorbar()
