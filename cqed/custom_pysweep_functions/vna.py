4b84715f3936f73001ad00e2e392a8b5f8b328a1
lgrunhaupt <l.grunhaupt@tudelft.nl>
2020-02-14 17:43:52 +0100

    add functions to measure multiple vna traces with individual centerfreq and spans

--- cqed/custom_pysweep_functions/vna.py
+++ cqed/custom_pysweep_functions/vna.py
@@ -25,3 +25,45 @@ def return_vna_trace(d):
     vna_data = station.vna.S21.trace_mag_phase()
 
     return [freqs, vna_data[0], vna_data[1]]
+
+
+def transmission_vs_frequency_explicit(center, span, suffix=None):
+    @MakeMeasurementFunction([DataParameter(name='frequency' + str(suffix),
+                                            unit='Hz',
+                                            paramtype='array',
+                                            # yes independent, but pysweep will not recognize it as such
+                                            independent=2
+                                            ),
+                              DataParameter(name='amplitude' + str(suffix),
+                                            unit='',
+                                            paramtype='array',
+                                            # explicitely tell that this parameter depends on
+                                            # the corresponding frequency parameter
+                                            extra_dependencies=['frequency' + str(suffix)]
+                                            ),
+                              DataParameter(name='phase' + str(suffix),
+                                            unit='rad',
+                                            paramtype='array',
+                                            # explicitely tell that this parameter depends on
+                                            # the corresponding frequency parameter
+                                            extra_dependencies=['frequency' + str(suffix)]
+                                            )
+                              ])
+    def transmission_vs_frequency_measurement_function(d):
+        station = d['STATION']
+        station.vna.S21.center(center)
+        station.vna.S21.span(span)
+        return return_vna_trace(d)
+
+    # return a measurement function
+    return transmission_vs_frequency_measurement_function
+
+
+def multiple_meas_functions(freq_list, span_list):
+    fun_str = ''
+
+    for i, c in enumerate(freq_list):
+        s = span_list[i]
+        fun_str += 'cvna.transmission_vs_frequency_explicit({}, {}, suffix={})+'.format(c, s, i)
+    fun_str = fun_str[:-1]
+    return fun_str
