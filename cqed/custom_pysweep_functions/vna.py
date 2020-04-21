c614f852d982a05683c1ac5a043c2ed5facdf1fd
Arno Bargerbos <abargerbos@tudelft.net>
2020-04-21 15:56:49 +0200

    Added function to set uop VNA for frequency sweep

--- cqed/custom_pysweep_functions/vna.py
+++ cqed/custom_pysweep_functions/vna.py
@@ -7,14 +7,51 @@ from pysweep.core.measurementfunctions import MakeMeasurementFunction
 from pysweep.databackends.base import DataParameter
 import numpy as np
 
+def setup_frq_sweep(station, fstart, fstop, fstep, chan='S21', bw=None, navgs=None, pwr=None, electrical_delay=None):
+    """
+    Function that sets up the VNA according to the specified parameters, leaving other parameters intact. 
+    Frequency parameters are required, others are optional.
+    Assumes that a channel with name `chan` is already created.
+
+    Args:
+        station: QCoDeS station that contains a R&S ZNB VNA instrument
+        fstart (Hz): starting frequency of VNA sweep
+        fstop (Hz): final frequency of VNA sweep
+        fstep (Hz): step size of VNA sweep
+        chan: name of VNA channel to be used
+        bw (Hz): VNA bandwidth
+        navgs: number of averages per measurement
+        pwr (dBm): VNA power
+        electrical_delay (s): electrical delay used by the VNA
+    """
+
+    vna_trace = getattr(station.vna.channels, chan)
+    
+    if navgs is None:
+        navgs = vna_trace.avg()
+    if bw is None:
+        bw = vna_trace.bandwidth()
+    if pwr is None:
+        pwr = vna_trace.power()  
+    if electrical_delay is None:
+        electrical_delay = vna_trace.electrical_delay()
+
+    vna_trace.start(int(fstart))
+    vna_trace.stop(int(fstop))
+    vna_trace.npts(int((fstop-fstart)/fstep))
+    vna_trace.bandwidth(bw)
+    vna_trace.power(pwr)
+    vna_trace.avg(navgs)
+    vna_trace.electrical_delay()
+
 @MakeMeasurementFunction([DataParameter('frequency','Hz', 'array', True),
                           DataParameter('amplitude', '', 'array'), 
                           DataParameter('phase', 'rad', 'array')])
 def return_vna_trace(d):
     """
     Pysweep VNA measurement function.
-    Returns VNA frequency axis, linear amplitude and phase in radians.
-    Keeps currently set VNA paramaters.
+    Returns VNA frequency axis in Hz, linear amplitude in ? and phase in radians.
+    Keeps currently set VNA parameters.
     """
     station = d['STATION']
     freqs = np.linspace(station.vna.S21.start(), station.vna.S21.stop(), station.vna.S21.npts())
