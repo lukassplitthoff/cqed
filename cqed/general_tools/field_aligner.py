cbb9156a438bc69b343fbd1e70261e9030d85b38
Arno <arnobargerbos@hotmail.com>
2020-04-23 15:39:00 +0200

    Fixes to field aligner

--- cqed/general_tools/field_aligner.py
+++ cqed/general_tools/field_aligner.py
@@ -29,17 +29,18 @@ class FieldAligner(object):
         self.magnet = instrument
         self.objective = objective
 
-    def optimize_x(self,
-                   initial_step=200e-6,
-                   final_step=50e-6,
-                   resolution=20e-6,
-                   initial_direction=1,
-                   max_amplitude=10e-3,
-                   waiting_time=3,
-                   return_extra=False,
-                   plot=False,
-                   verbose=False
-                   ):
+    def optimize_x(
+        self,
+        initial_step=200e-6,
+        final_step=50e-6,
+        resolution=20e-6,
+        initial_direction=1,
+        max_amplitude=10e-3,
+        waiting_time=3,
+        return_extra=False,
+        plot=False,
+        verbose=False,
+    ):
         """At fixed magnitude of y and z field, maximizes the objective by wiggling
         the x-axis of the magnetic field up and down in decreasing to deal with
         hysteresis, a common pitfall encountered when using an exhaustive search.
@@ -74,8 +75,7 @@ class FieldAligner(object):
 
         nsteps = 1
         direction = initial_direction
-        wiggle_step = resolution * \
-            round(float(initial_step) / resolution)
+        wiggle_step = resolution * round(float(initial_step) / resolution)
         current_objective = self.objective()
         current_pos = self.magnet.x_measured()
 
@@ -84,30 +84,19 @@ class FieldAligner(object):
 
         while wiggle_step >= final_step:
 
-            if abs(
-                    current_pos +
-                    direction *
-                    wiggle_step) > max_amplitude:
-                raise Exception(
-                    'X-axis magnitude limit reached.')
+            if abs(current_pos + direction * wiggle_step) > max_amplitude:
+                raise Exception("X-axis magnitude limit reached.")
 
             if verbose:
                 print(
-                    'f0 = {0:.4f} GHz, sweeping from '.format(
-                        current_objective /
-                        1e9) +
-                    '{0:.4f} mT to '.format(
-                        current_pos *
-                        1e3) +
-                    '{0:.4f} mT'.format(
-                        (current_pos +
-                         direction *
-                         wiggle_step) *
-                        1e3))
+                    "f0 = {0:.4f} GHz, sweeping from ".format(current_objective / 1e9)
+                    + "{0:.4f} mT to ".format(current_pos * 1e3)
+                    + "{0:.4f} mT".format((current_pos + direction * wiggle_step) * 1e3)
+                )
 
-            self.magnet.x_target(
-                current_pos + direction * wiggle_step)
-            self.magnet.x_source.ramp_to_target()
+            # self.magnet.x_target(
+            #     current_pos + direction * wiggle_step)
+            # self.magnet.ramp(mode='safe')
             sleep(waiting_time)
 
             new_pos = self.magnet.x_measured()
@@ -122,15 +111,13 @@ class FieldAligner(object):
                 current_pos = new_pos
                 direction = -1 * direction
                 if verbose:
-                    print('Turning around')
+                    print("Turning around")
 
-                if np.sign(direction) == np.sign(
-                        initial_direction):
-                    wiggle_step = wiggle_step / 2.
-                    wiggle_step = resolution * \
-                        round(float(wiggle_step) / resolution)
+                if np.sign(direction) == np.sign(initial_direction):
+                    wiggle_step = wiggle_step / 2.0
+                    wiggle_step = resolution * round(float(wiggle_step) / resolution)
                     if verbose:
-                        print('Halving step size')
+                        print("Halving step size")
             else:
                 current_objective = new_objective
                 current_pos = new_pos
@@ -142,67 +129,64 @@ class FieldAligner(object):
 
         optimum = objectives_meas[-1]
 
-        print('Optimization finished after {} steps'.format(nsteps),
-              '\nTarget value from {0:.4f} GHz to '.format(
-                  objectives_meas[0] / 1e9) + '{0:.4f} GHz'.format(optimum / 1e9),
-              '\nFinal X-field = {0:.3f} mT'.format(new_pos * 1e3))
+        print(
+            "Optimization finished after {} steps".format(nsteps),
+            "\nTarget value from {0:.4f} GHz to ".format(objectives_meas[0] / 1e9)
+            + "{0:.4f} GHz".format(optimum / 1e9),
+            "\nFinal X-field = {0:.3f} mT".format(new_pos * 1e3),
+        )
 
-        extra = {
-            'objectives': objectives_meas,
-            'fields': locations
-        }
+        extra = {"objectives": objectives_meas, "fields": locations}
 
         if plot:
-            plt_xs = np.array(extra['fields'])
-            plt_ys = np.array(extra['objectives'])
+            plt_xs = np.array(extra["fields"])
+            plt_ys = np.array(extra["objectives"])
             plt.figure()
             plt.plot(plt_xs * 1e3, plt_ys)
             plt.scatter(plt_xs * 1e3, plt_ys, c=plt_ys)
             plt.colorbar()
-            plt.xlabel('X field [mT]')
-            plt.ylabel('Objective [GHz')
+            plt.xlabel("X field [mT]")
+            plt.ylabel("Objective [GHz]")
 
         if return_extra:
             return optimum, extra
         else:
             return optimum
 
-    def optimize_and_ramp_r(self,
-                            rs,
-                            initial_theta,
-                            initial_phi,
-                            optimize_first,
-                            optimize_strategy=None,
-                            optimize_at=None,
-                            reoptimization_threshold=None,
-                            waiting_time=3,
-                            max_field_strength=1.5,
-                            return_extra=True,
-                            verbose=True,
-                            **kwargs
-                            ):
-        """Ramps the magnetic field over the magnitude values provided, optimizing
+    def optimize_and_ramp_r(
+        self,
+        rs,
+        initial_theta,
+        initial_phi,
+        optimize_first=False,
+        optimize_strategy=None,
+        optimize_at=None,
+        reoptimization_threshold=None,
+        waiting_time=3,
+        max_field_strength=1.5,
+        return_extra=True,
+        verbose=True,
+        **kwargs,
+    ):
+        """Ramps the magnetic field to the given magnitudes provided, optimizing
         the objective by adjusting the x-field whenever required as given by
         `optimize_strategy`. Use the **kwargs to set up optimize_x!
 
-        Args: rs (T): Array of magnitude values along which the field is
-        ramped. Smaller steps implies less risk of losing resonances but takes
-        more time.
-        initial_theta (deg): intial angle theta used to set the
-        direction of the field.
-        initial_phi (deg): intial angle phi used to set
-        the direction of the field.
+        Args: rs (T): Array of magnitude values
+        initial_theta (deg): intial angle theta 
+        initial_phi (deg): intial angle phi 
         optimize_first (boolean): Whether to
-        optimize the field before starting the ramp procedure. Note that this
-        might overwrite the input values of theta and phi!
-        optimize_strategy (str): three options are implemented:
-        `objective_decrease`, for which one optimizes the alignment if the objective
+        optimize the field after ramping to the first field value. 
+        optimize_strategy (str): four options are implemented:
+        `objective_decrease` optimizes the alignment if the objective
         has decreased by more than `reoptimization_threshold` since the last
-        call to `optimize_x`. The next option is `optimize_at_fields`,
-        for which the alignment is optimized only at the field given by `optimize_at`.
+        call to `optimize_x`. 
+        `optimize_at_fields` aligns only if the field is in the array `optimize_at`.
+        `always` aligns every time after the field has been ramped
         And finally, if `None` is chosen, the alignment is never optimized.
-        optimize_at (T): Array of values at which the alignment will be optimized if
-        `optimize_strategy` = `optimize_at`
+
+        optimize_at (T): single value or array of values at which the alignment will be optimized if
+        `optimize_strategy` = `optimize_at_fields`
         reoptimization_threshold (GHz): Value against which the change in objective is
         compared in order to establish if the field has to be aligned if
         `optimize_strategy` = `objective_decrease`.
@@ -212,27 +196,30 @@ class FieldAligner(object):
         data as a dictionary.
         verbose (boolean): toggles print statements during optimization and
         sweeping.
-        Returns:     The optimal field value found.     If
-        `return_extra=True`, this method returns a tuple of the
+        Returns:     The optimal field value found.     
+        If `return_extra=True`, this method returns a tuple of the
         optimal field and a dictionary containing diagnostic data.
 
         """
-
         if max_field_strength > 1.5:
             showwarning(
-                'Be aware that mu-metal shields are saturated by too large magnetic fields and will not work afterwards. '
-                'Are you sure you want to go to more than 1.5 T?',
+                "Be aware that mu-metal shields are saturated by too large magnetic fields and will not work afterwards. "
+                "Are you sure you want to go to more than 1.5 T?",
                 ResourceWarning,
-                'cqed/cqed/custom_pysweep_functions/field_aligner',
-                193)
-        if optimize_strategy == 'objective_decrease':
+                "cqed/cqed/custom_pysweep_functions/field_aligner",
+                193,
+            )
+        if optimize_strategy == "objective_decrease":
             if reoptimization_threshold is None:
                 raise Exception(
-                    'You need to specify a reoptimization_threshold when using the `objective_decrease` option.')
-        elif optimize_strategy == 'optimize_at_fields':
+                    "You need to specify a reoptimization_threshold when using the `objective_decrease` option."
+                )
+        elif optimize_strategy == "optimize_at_fields":
             if optimize_at is None:
                 raise Exception(
-                    'You need to specify at which fields to optimize when using the `optimize_at_fields` option.')
+                    "You need to specify at which fields to optimize when using the `optimize_at_fields` option."
+                )
+            optimize_at = np.atleast_1d(optimize_at) #if single number, make into array of single number
 
         extra = {}
         objective_history = []
@@ -241,18 +228,9 @@ class FieldAligner(object):
         self.theta = initial_theta
         self.phi = initial_phi
 
-        if optimize_first:
-            current_objective = self.optimize_x(
-                verbose=verbose,
-                return_extra=False,
-                waiting_time=waiting_time,
-                **kwargs)
-        else:
-            current_objective = self.objective()
-
-        last_optimized = current_objective
-
-        for r in rs:
+        current_objective = self.objective()
+        self.last_optimized = current_objective
+        for ii, r in enumerate(rs):
             current_x = self.magnet.x_measured()
             current_y = self.magnet.y_measured()
             current_z = self.magnet.z_measured()
@@ -260,83 +238,89 @@ class FieldAligner(object):
             objective_history.append(current_objective)
             optima_history.append([self.theta, self.phi])
 
-            new_x = r * np.sin(np.radians(self.theta)) * \
-                np.cos(np.radians(self.phi))
-            new_y = r * np.sin(np.radians(self.theta)) * \
-                np.sin(np.radians(self.phi))
+            new_x = r * np.sin(np.radians(self.theta)) * np.cos(np.radians(self.phi))
+            new_y = r * np.sin(np.radians(self.theta)) * np.sin(np.radians(self.phi))
             new_z = r * np.cos(np.radians(self.theta))
 
-            if np.sqrt(
-                    new_x**2 +
-                    new_y**2 +
-                    new_z**2) > max_field_strength:
-                raise Exception(
-                    'Target field exceeds max field strength.')
+            if np.sqrt(new_x ** 2 + new_y ** 2 + new_z ** 2) > max_field_strength:
+                raise Exception("Target field exceeds max field strength.")
 
             if verbose:
-                print(
-                    f"Sweeping x-axis field from {current_x} to {new_x}")
-                print(
-                    f"Sweeping y-axis field from {current_y} to {new_y}")
-                print(
-                    f"Sweeping z-axis field from {current_z} to {new_z}")
+                print(f"Sweeping x-axis field from {current_x} to {new_x}")
+                print(f"Sweeping y-axis field from {current_y} to {new_y}")
+                print(f"Sweeping z-axis field from {current_z} to {new_z}")
 
-            self.magnet.x_target(new_x)
-            self.magnet.y_target(new_y)
-            self.magnet.z_target(new_z)
+            # self.magnet.x_target(new_x)
+            # self.magnet.y_target(new_y)
+            # self.magnet.z_target(new_z)
+            # self.magnet.ramp(mode='safe')
             sleep(waiting_time)
-
             if verbose:
                 print(f"Sweep finished.")
-
             new_objective = self.objective()
-            objective_history.append(new_objective)
 
-            if optimize_strategy == 'objective_decrease':
-                distance = last_optimized - new_objective
+            if optimize_first and ii == 0:
+                print("First iteration, optimizing.")
+                current_objective = self.optimize_x(
+                    verbose=verbose,
+                    return_extra=False,
+                    waiting_time=waiting_time,
+                    **kwargs,
+                )
+                self.last_optimized = current_objective
+
+            elif optimize_strategy == "objective_decrease":
+                distance = self.last_optimized - new_objective
                 if distance < reoptimization_threshold:
                     current_objective = new_objective
                     print(
-                        f"Shift within {reoptimization_threshold}, not re-optimizing.")
+                        f"Shift within {reoptimization_threshold}, not re-optimizing."
+                    )
                 else:
                     print(
-                        f"Current and previous objectives differ by {distance}, thus re-optimizing.")
+                        f"Current and previous objectives differ by {distance}, thus re-optimizing."
+                    )
                     current_objective = self.optimize_x(
                         verbose=verbose,
                         return_extra=False,
                         waiting_time=waiting_time,
-                        **kwargs)
-                    last_optimized = current_objective
+                        **kwargs,
+                    )
+                    self.last_optimized = current_objective
 
-            elif optimize_strategy == 'optimize_at_fields':
-                if np.any(
-                    np.isclose(
-                        r,
-                        optimize_at,
-                        atol=np.diff(rs)[0] /
-                        2)):
-                    print(
-                        f"Current field is in `optimize_at`, thus re-optimizing.")
+            elif optimize_strategy == "optimize_at_fields":
+                if np.any(np.isclose(r, optimize_at, atol=5e-4)):
+                    print(f"Current field is in `optimize_at`, thus re-optimizing.")
                     current_objective = self.optimize_x(
                         verbose=verbose,
                         return_extra=False,
                         waiting_time=waiting_time,
-                        **kwargs)
+                        **kwargs,
+                    )
                 else:
                     current_objective = new_objective
+
+            elif optimize_strategy == "always":
+                print("Optimizing.")
+                current_objective = self.optimize_x(
+                    verbose=verbose,
+                    return_extra=False,
+                    waiting_time=waiting_time,
+                    **kwargs,
+                )
 
             objective_history.append(current_objective)
             optima_history.append([self.theta, self.phi])
 
-            if return_extra:
-                extra['history'] = {
-                    'rs': rs,
-                    'objectives': objective_history,
-                    'optima': optima_history
-                }
-                return objective_history[-1], extra
-            else:
-                return objective_history[-1]
+        if return_extra:
+            extra["history"] = {
+                "rs": rs,
+                "objectives": objective_history,
+                "optima": optima_history,
+            }
+            return objective_history[-1], extra
+        else:
+            return objective_history[-1]
 
     def magnet_components_sph(self):
         """Return the x, y, z component of the magnet, convert to spherical using the
@@ -345,12 +329,11 @@ class FieldAligner(object):
         <--> (x, y, z) definition and return as list.
 
         """
-        x = self.magnet.x_measured() + \
-            1e-9  # avoiding dividing by true zero
+        x = self.magnet.x_measured() + 1e-9  # avoiding dividing by true zero
         y = self.magnet.y_measured() + 1e-9
         z = self.magnet.z_measured() + 1e-9
-        
-        r_meas = np.sqrt(x**2 + y**2 + z**2)
+
+        r_meas = np.sqrt(x ** 2 + y ** 2 + z ** 2)
         phi_meas = np.arctan2(y, x) / np.pi * 180
         if phi_meas < 0:
             phi_meas += 360  # convention is 0 to 360 not -180 to 180
