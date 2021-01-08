3c5e56593e4648acf106d6b9624066b93128d02f
Arno Bargerbos <arnobargerbos@hotmail.com>
2021-01-08 16:01:30 +0100

    changed peakfinder from minimum to min or max

--- cqed/general_tools/peak_finding.py
+++ cqed/general_tools/peak_finding.py
@@ -2,7 +2,7 @@
 # 22-APR-2020
 
 """
-A set of functions used to find minima in (potentially noisy) data.
+A set of functions used to find extrema in (potentially noisy) data.
 
 Would be good to identify some other options, but ultimately it will depend on the quality of the data and is thus a bit user dependent.
 """
@@ -10,52 +10,65 @@ Would be good to identify some other options, but ultimately it will depend on t
 import numpy as np
 from scipy import signal
 
-
-def find_minimum_absolute(fs, ys):
+def _reflect_in_mean(ys):
     """ 
-    Finds the frequency belonging to the minimal amplitude of a trace.
+    Function to change a dip into a peak, so that we can also look for peaks in data with dips
+    """
+    return -ys+2*np.mean(ys)
+
+def _round_up_to_odd(f):
+    return int(np.ceil(f) // 2 * 2 + 1)
+
+def _convert_to_dBm(ys):
+    return 20 * np.log10(ys)
+
+def find_peak_literal(fs, ys, reflect=False, dBm=False):
+    """ 
+    Finds the frequency belonging to the literal maximal amplitude of a trace.
 
     fs (any): array of x values
     ys (any) array of y values
     
     returns:
-    value of fs that corresponds to the minimum of ys
+    value of fs that corresponds to the maximum of ys
     """
-    return fs[np.argmin(ys)]
+
+    if reflect==True:
+        ys = _reflect_in_mean(ys)
+    if dBm==True:
+        ys = _convert_to_dBm(ys)
+
+    return fs[np.argmax(ys)]
 
 
-def find_minimum_filtered(fs, ys, window=1e6, prominence=4):
+def find_peak_filtered(fs, ys, window=1e6, prominence=4):
 
-    """ Taking the minimum of a trace to find a resonance is prone to noise. 
+    """ Taking the maximum of a trace to find a resonance is prone to noise. 
     We therefore filter the data based on a savgol filter with a window not 
-    smaller than the linewidth of interest. We then look for dips using 
+    smaller than the linewidth of interest. We then look for peaks using 
     prominence based peak finding.
 
     args:
     fs (any): array of x values
     ys (any) array of y values
     window (same as fs): window used to filter data. Should not be larger than resonance linewidth of interest.
-    prominence (same as ys): the topographic prominense a dip needs to have to be considered a dip. 
+    prominence (same as ys): the topographic prominense a peak needs to have to be considered a peak. 
     
     returns:
-    value of fs that corresponds to the identified minimum in ys
+    value of fs that corresponds to the identified maximum in ys
     Needs testing. 
     """
 
     min_peakwidth = window / (fs[1] - fs[0])
     yhat = signal.savgol_filter(ys, max(_round_up_to_odd(min_peakwidth * 3), 3), 2) #window length must be odd and >= polyorder
-    peaks, _ = signal.find_peaks(-1.0 * yhat, prominence=prominence)
+    peaks, _ = signal.find_peaks(yhat, prominence=prominence)
 
     if len(peaks) == 0:
         resfreq = None
     elif len(peaks) > 1:
-        peakarg = np.argmin(yhat[peaks])
+        peakarg = np.argmax(yhat[peaks])
         resfreq = fs[peaks[peakarg]]
     else:
         resfreq = fs[peaks][0]
 
     return resfreq
-
-
-def _round_up_to_odd(f):
-    return int(np.ceil(f) // 2 * 2 + 1)
